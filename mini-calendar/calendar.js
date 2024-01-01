83b3384d784a676d543cb2f034580ed99dc023de
Shakha Abdullaev <81807625+ShakhaAbdullaev@users.noreply.github.com>
2024-01-01 13:55:39 +0500

    Update calendar.js
    fix months' name

--- mini-calendar/calendar.js
+++ mini-calendar/calendar.js
@@ -1,4 +1,4 @@
-const months = ["Januar", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 let monthObj = new Date();
 let dayObj = new Date();
